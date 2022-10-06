29f120be0a1259363e8cfbe4a95418468eba4591
Katie <kruane2@depaul.edu>
2022-10-05 20:51:58 -0500

    Tests for initializing zero, adding 7, subtracting 10

--- src/test/java/com/depaul/gsd/CalculatorTestKatieRuane.java
+++ src/test/java/com/depaul/gsd/CalculatorTestKatieRuane.java
@@ -8,11 +8,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTestKatieRuane {
 
-    CalculatorClassActivity calculatorActivity;
+    CalculatorClassActivity calculatorClassActivity;
 
     @BeforeEach
     void setUp() {
-        calculatorActivity = new CalculatorClassActivity();
+        calculatorClassActivity = new CalculatorClassActivity();
     }
 
     @Test
@@ -21,7 +21,7 @@ class CalculatorTestKatieRuane {
     void calcGetValue() {
 
 //      This is an assert statement which validates that expected and actual value is the same, if not the message at the end of the method is shown
-        assertEquals(0, calculatorActivity.getValue(), "Initial value is zero");
+        assertEquals(0, calculatorClassActivity.getValue(), "Initial value is zero");
 
     }
 
@@ -29,9 +29,9 @@ class CalculatorTestKatieRuane {
     @DisplayName("Adding 7 from initial value")
 //    @Disabled("The method changed - Fix later")  //marks a test to be disabled.
     void addSeven() {
-        calculatorActivity.add(7);
+        calculatorClassActivityActivity.add(7);
 //      This is an assert statement which validates that expected and actual value is the same, if not the message at the end of the method is shown
-        assertEquals(7, calculatorActivity.getValue(), "Regular addition should work");
+        assertEquals(7, calculatorClassActivity.getValue(), "Regular addition should work");
 
     }
 
@@ -39,9 +39,9 @@ class CalculatorTestKatieRuane {
     @DisplayName("Subtracting 10 from initial value")
 //    @Disabled("The method changed - Fix later")  //marks a test to be disabled.
     void subtractTen() {
-        calculatorActivity.subtract(10);
+        calculatorClassActivity.subtract(10);
 //      This is an assert statement which validates that expected and actual value is the same, if not the message at the end of the method is shown
-        assertEquals(-10, calculatorActivity.getValue(), "Regular subtraction should work");
+        assertEquals(-10, calculatorClassActivity.getValue(), "Regular subtraction should work");
 
     }
 }
